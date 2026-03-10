6d7179c58629b6bbc25ecf9b9c4a87281233f8e4
Luke Vincent <luke.vincent@csiro.au>
2026-03-10 14:22:25 +1100

    Update generated API files to new version

--- gen/dashboard/service.go
+++ gen/dashboard/service.go
@@ -39,7 +39,7 @@ type Auther interface {
 const APIName = "ivcap"
 
 // APIVersion is the version of the API as defined in the design.
-const APIVersion = "0.47"
+const APIVersion = "0.48"
 
 // ServiceName is the name of the service as defined in the design. This is the
 // same value that is set in the endpoint request contexts under the ServiceKey
